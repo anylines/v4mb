12dbb2f559f26db0f41f27e466235a563601f74b
AnyLine <859882067@qq.com>
2021-05-18 23:19:36 -0500

    Update v4mb.sh

--- v4mb.sh
+++ v4mb.sh
@@ -11,4 +11,4 @@ npm i
 pm2 start server.js
 echo -e "默认http端口号为5678，如果你做了映射，请使用实际映射的端口进行访问。
 默认用户名admin，密码adminadmin。
-如果npm i安装出现警告，请在容器重启时候,进入容器终端并输入pm2 start /jd/panel/server.js以启动面板，否则面板将无法正常访问！！！"
+！！！如果npm i安装出现警告，请在容器重启时候,进入容器终端并输入pm2 start /jd/panel/server.js以启动面板，否则面板将无法正常访问！！！"
